1df80511e894295c643e8410612417943fefb426
sriki <sriki77@gmail.com>
2011-02-21 11:33:43 +0530

    removed generated files

--- src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/srikanth/wip/eclispe_workspace/cpp_ccn/src
-# Build directory: /home/srikanth/wip/eclispe_workspace/cpp_ccn/src
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
